11848b9d4e805f7fca4fe61d01573acca805692e
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-07-21 23:24:17 +0400

    fa

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+
+        int fourthDigit = number % 10;
+        int thirdDigit = (number / 10) % 10;
+        int secondDigit = (number / 100) % 10;
+        int firstDigit = number / 1000;
+
+        int sumOfDigits = firstDigit + secondDigit + thirdDigit + fourthDigit;
+
+        System.out.println(sumOfDigits);
     }
 }
